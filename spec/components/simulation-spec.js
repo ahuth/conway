88a123a3cd538dc6881e73583e7aebb31d4043ee
Andrew <andrew@huth.me>
2016-07-16 12:57:08 -0700

    Update <Simulation> tests to cover the dispatching of actions
    Everything is still shallow rendered, and I’m not sure what the best
    way to test this is.

--- spec/components/simulation-spec.js
+++ spec/components/simulation-spec.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Simulation from "components/simulation";
+import store from "store";
+import {setSize} from "actions";
 import {shallow} from "enzyme";
 
 describe("components", function () {
@@ -10,12 +12,25 @@ describe("components", function () {
       component = shallow(<Simulation />);
     });
 
-    it("has a slider", function () {
-      expect(component.find("Slider").length).toBe(1);
-    });
-
     it("has a grid", function () {
       expect(component.find("Grid").length).toBe(1);
+    });
+
+    describe("the slider", function () {
+      it("exists", function () {
+        expect(component.find("Slider").length).toBe(1);
+      });
+
+      describe("on change", function () {
+        beforeEach(function () {
+          spyOn(store, "dispatch").and.callThrough()
+          component.find("Slider").simulate("change", {target: {value: "10"}});
+        });
+
+        it("dispatches the correct action", function () {
+          expect(store.dispatch).toHaveBeenCalledWith(setSize(10));
+        });
+      });
     });
   });
 });
