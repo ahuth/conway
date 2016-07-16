9e6b2c122d5db1e0155e26564fd67780237aa15d
Andrew <andrew@huth.me>
2016-07-16 11:13:55 -0700

    Setup Redux store in the <Simulation> component and pass the size down to the grid

--- app/components/simulation.js
+++ app/components/simulation.js
@@ -3,13 +3,30 @@
 import Grid from "./grid";
 import React from "react";
 import Slider from "./slider";
+import store from "../store";
 
 var Simulation = React.createClass({
+  getInitialState: function () {
+    return store.getState();
+  },
+
+  componentWillMount: function () {
+    this.unsubscribe = store.subscribe(this.onChange);
+  },
+
+  componentWillUnmount: function () {
+    this.unsubscribe();
+  },
+
+  onChange: function () {
+    this.setState(store.getState());
+  },
+
   render: function () {
     return (
       <div>
         <Slider />
-        <Grid height={10} width={10} />
+        <Grid height={this.state.size.height} width={this.state.size.width} />
       </div>
     );
   }
