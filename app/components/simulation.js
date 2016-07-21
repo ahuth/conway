824e0a1770cc5185a163464a5b328018b7aac056
Andrew <andrew@huth.me>
2016-07-20 19:21:40 -0700

    Add play button to the simulation!

--- app/components/simulation.js
+++ app/components/simulation.js
@@ -1,9 +1,10 @@
 "use strict";
 
+import Button from "./button";
 import Grid from "./grid";
 import React from "react";
 import store from "../store";
-import {toggleCell} from "../actions";
+import {processWorld, toggleCell} from "../actions";
 
 var Simulation = React.createClass({
   getInitialState: function () {
@@ -26,9 +27,14 @@ var Simulation = React.createClass({
     store.dispatch(toggleCell(row, column));
   },
 
+  onPlayClick: function () {
+    store.dispatch(processWorld());
+  },
+
   render: function () {
     return (
       <div>
+        <Button onClick={this.onPlayClick} text={"▶"} />
         <Grid cells={this.state.cells} onCellClick={this.onCellClick} />
       </div>
     );
--- spec/components/simulation-spec.js
+++ spec/components/simulation-spec.js
@@ -4,7 +4,7 @@ import React from "react";
 import Simulation from "components/simulation";
 import store from "store";
 import {shallow} from "enzyme";
-import {toggleCell} from "actions";
+import {processWorld, toggleCell} from "actions";
 
 describe("components", function () {
   describe("<Simulation />", function () {
@@ -27,6 +27,24 @@ describe("components", function () {
 
         it("dispatches the correct action", function () {
           var action = toggleCell(6, 7);
+          expect(store.dispatch).toHaveBeenCalledWith(action);
+        });
+      });
+    });
+
+    describe("the play button", function () {
+      it("exists", function () {
+        expect(component.find("Button").length).toBe(1);
+      });
+
+      describe("clicking", function () {
+        beforeEach(function () {
+          spyOn(store, "dispatch").and.callThrough();
+          component.find("Button").simulate("click");
+        });
+
+        it("dispatches the correct action", function () {
+          var action = processWorld();
           expect(store.dispatch).toHaveBeenCalledWith(action);
         });
       });
