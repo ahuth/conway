0c5ff57aa5a2e9286be9000ec43cb5966492dab3
Andrew <andrew@huth.me>
2016-07-20 19:04:47 -0700

    Add action and reducer to process the current frame of the world

--- app/reducers/cells.js
+++ app/reducers/cells.js
@@ -2,12 +2,15 @@
 
 import clone from "clone";
 import initialState from "./initial-state";
+import process from "../utils/process";
 import {actionTypes} from "../constants";
 
 function cells(state = initialState.cells, action) {
   switch (action.type) {
     case actionTypes.CELL_TOGGLE:
       return toggleCell(clone(state), action);
+    case actionTypes.WORLD_PROCESS:
+      return process(state);
     default:
       return state;
   }
