dc4745a7c9d16184bff939764cfa69a38287fff0
Andrew <andrew@huth.me>
2017-09-25 19:54:20 -0700

    Use a Component class instead of `createClass`

--- src/components/simulation.js
+++ src/components/simulation.js
@@ -4,58 +4,56 @@ import React from "react";
 import store from "../store";
 import {clearWorld, processWorld, randomizeWorld, startWorld, stopWorld, toggleCell} from "../actions";
 
-const Simulation = React.createClass({
-  getInitialState: function () {
-    return store.getState();
-  },
+export default class Simulation extends React.Component {
+  state = store.getState();
 
-  componentWillMount: function () {
+  componentDidMount() {
     this.unsubscribe = store.subscribe(this.onChange);
     this.props.timer(this.onTick);
-  },
+  }
 
-  componentWillUnmount: function () {
+  componentWillUnmount() {
     this.unsubscribe();
-  },
+  }
 
-  onChange: function () {
-    this.setState(store.getState());
-  },
-
-  onCellClick: function (row, column) {
+  onCellClick(row, column) {
     store.dispatch(toggleCell(row, column));
-  },
+  }
 
-  onClearClick: function () {
+  onClearClick() {
     store.dispatch(clearWorld());
     store.dispatch(stopWorld());
-  },
+  }
 
-  onPlayClick: function () {
+  onRandomClick() {
+    store.dispatch(randomizeWorld());
+    store.dispatch(stopWorld());
+  }
+
+  onStepClick() {
+    store.dispatch(processWorld());
+  }
+
+  onChange = () => {
+    this.setState(store.getState());
+  }
+
+  onTick = () => {
+    if (this.state.running) {
+      store.dispatch(processWorld());
+    }
+    this.props.timer(this.onTick);
+  }
+
+  onPlayClick = () => {
     if (this.state.running) {
       store.dispatch(stopWorld());
     } else {
       store.dispatch(startWorld());
     }
-  },
+  }
 
-  onRandomClick: function () {
-    store.dispatch(randomizeWorld());
-    store.dispatch(stopWorld());
-  },
-
-  onStepClick: function () {
-    store.dispatch(processWorld());
-  },
-
-  onTick: function () {
-    if (this.state.running) {
-      store.dispatch(processWorld());
-    }
-    this.props.timer(this.onTick);
-  },
-
-  render: function () {
+  render() {
     return (
       <div>
         <Button onClick={this.onStepClick} text={"Step"} />
@@ -66,6 +64,4 @@ const Simulation = React.createClass({
       </div>
     );
   }
-});
-
-export default Simulation;
+}
