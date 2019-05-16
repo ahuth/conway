9cb099b98e47f6bf2f309ba9572e5e784fe3d27d
Andrew Huth <andrew@huth.me>
2019-05-15 17:05:45 -0700

    Add slider to control the speed the simulation runs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,9 @@ import Grid from './Grid';
 import useSimulation from '../hooks/useSimulation';
 
 export default function App() {
-  const { world, playing, doClear, doRandomize, doStep, doToggleCell, doToggleStart } = useSimulation(50);
   const [showGridLines, setShowGridLines] = useState(true);
+  const [delay, setDelay] = useState(50);
+  const { world, playing, doClear, doRandomize, doStep, doToggleCell, doToggleStart } = useSimulation(delay);
 
   return (
     <div>
@@ -15,6 +16,10 @@ export default function App() {
       <label>
         Show grid lines:
         <input type="checkbox" onChange={() => setShowGridLines(state => !state)} checked={showGridLines} />
+      </label>
+      <label>
+        Delay:
+        <input type="range" min="50" max="500" step="50" onChange={(e) => setDelay(Number(e.target.value))} value={delay} />
       </label>
       <Grid onClick={doToggleCell} showGridLines={showGridLines} world={world} />
     </div>
