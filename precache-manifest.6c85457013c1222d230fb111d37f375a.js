d070c1478aa49af58c73ae0c6bf6dd4720a9b76b
Andrew Huth <andrew@huth.me>
2019-05-15 19:28:08 -0700

    Updates

--- precache-manifest.6c85457013c1222d230fb111d37f375a.js
+++ precache-manifest.6c85457013c1222d230fb111d37f375a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1e1aeef1918c3d828ae80d182daa4d7",
+    "revision": "6f0a784dc7a5547fa3655c3612eb0729",
     "url": "/conway/index.html"
   },
   {
-    "revision": "3d0e77654c70aa76fae2",
+    "revision": "8d7b490fe73b487dda2b",
     "url": "/conway/static/css/main.d8037b08.chunk.css"
   },
   {
-    "revision": "2bcf714c17049deaef91",
-    "url": "/conway/static/js/2.0c1ec166.chunk.js"
+    "revision": "19e7aef03e15e654a48c",
+    "url": "/conway/static/js/2.27e3d94f.chunk.js"
   },
   {
-    "revision": "3d0e77654c70aa76fae2",
-    "url": "/conway/static/js/main.a979854d.chunk.js"
+    "revision": "8d7b490fe73b487dda2b",
+    "url": "/conway/static/js/main.a7855236.chunk.js"
   },
   {
     "revision": "077235e42b82b172086d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/conway/precache-manifest.f7eda8da092fa08efdd93a10317d2a6d.js"
+  "/conway/precache-manifest.6c85457013c1222d230fb111d37f375a.js"
 );
 
 self.addEventListener('message', (event) => {
